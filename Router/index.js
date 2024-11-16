f6c90d6293fa8b09ac5129a0bc7e83523e947407
CalvinCandra <willycandra850@gmail.com>
2024-11-16 20:20:58 +0800

    API CRUD ADMIN

--- Router/index.js
+++ Router/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authrouter from "./AuthRouter/authrouter.js";
+import userrouter from "./UserRouter/userrouter.js";
 
 const Router = express();
 const api = "/api/v1";
 
 Router.use(api, authrouter);
+Router.use(api, userrouter);
 
 export default Router;
